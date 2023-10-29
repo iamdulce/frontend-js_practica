ab12935283410eb486a368786c12478b19561e33
Dulce Flores <dulce.1201@gmail.com>
2023-10-29 16:27:11 +0100

    styling product-list

--- products-list/productsListView.js
+++ products-list/productsListView.js
@@ -4,10 +4,14 @@ export const emptyProducts = () => {
 
 export const buildProducts = product => {
     return `
-    <img src="${product.image}" alt="product image"/>
-    <span>${product.name}</span>
-    <span>${product.description}</span>
-    <p>${product.price}</p>
-    <p>${product.status}</p>
+    <div class="product-image_container">
+        <img src="${product.image}" alt="product image" onerror="this.src='https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg'"/>
+    </div>
+    <div class="product-text_container">
+        <h4 class="product-name">${product.name}</h4>
+        <p class="product-description">${product.description}</p>
+        <span class="product-price">${product.price} €</span>
+        <span class="product-status">${product.status}</span>
+    </div
     `;
 };
